eb6d4d2e6163eea5f5d49894e529045981a980d5
PrashanthAmbure <p.soccer10@gmail.com>
2015-01-08 18:25:41 +0530

    Updated the code to have loggers and handled exceptions

--- src/main/java/com/pramati/core/WebCrawler.java
+++ src/main/java/com/pramati/core/WebCrawler.java
@@ -4,12 +4,15 @@
 package com.pramati.core;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.net.UnknownHostException;
 
 import org.apache.log4j.Logger;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
+import com.pramati.core.services.ApacheMavenWebCrawlerServiceImpl;
 import com.pramati.core.services.WebCrawlerService;
-import com.pramati.core.services.WebCrawlerServiceImpl;
 import com.pramati.core.util.PropsUtilities;
 
 /**
@@ -18,12 +21,40 @@ import com.pramati.core.util.PropsUtilities;
  */
 public class WebCrawler 
 {
-	public static Logger log = Logger.getLogger(WebCrawler.class);
+	private static final Logger log = Logger.getLogger(WebCrawler.class);
 	public static void main(String[] args) throws IOException
 	{
-		WebCrawlerService webCrawlerService = new WebCrawlerServiceImpl();
-		PropsUtilities props = new PropsUtilities();
-		Document docHTML = webCrawlerService.loadHTMLDocument(props.fetchPropValue("URL"));
-		webCrawlerService.navigateHTMLDocument(docHTML);
+		log.info("Crawling has started...");
+		try
+		{
+			WebCrawlerService webCrawlerService = new ApacheMavenWebCrawlerServiceImpl();
+			PropsUtilities props = new PropsUtilities();
+			Document docHTML = webCrawlerService.loadHTMLDocument(props.fetchPropValue("URL"));
+			Element tableElement;
+			try 
+			{
+				tableElement = webCrawlerService.getTableFromHTMLDocument(docHTML, "grid", "2014");
+				webCrawlerService.processHTMLTableAndDownloadEmails(tableElement);
+				log.info("Crawling finished successfully...");
+			} 
+			catch (ValidationException e) 
+			{
+				log.error("Crawling finished with errors..."+e);
+			}
+		}
+		catch(UnknownHostException e)
+		{
+			log.info("No Internet Connection !!");
+			log.error("Crawling finished with errors..."+e);
+		}
+		catch(SocketTimeoutException e)
+		{
+			log.info("No Internet Connection !!");
+			log.error("Crawling finished with errors..."+e);
+		}
+		catch(Exception e)
+		{
+			log.error("Crawling finished with errors..."+e);
+		}
 	}
 }
