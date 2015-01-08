55477f717da620109ae1b9db276e49b99ccd153e
PrashanthAmbure <p.soccer10@gmail.com>
2015-01-08 18:31:12 +0530

    Created a user defined exception

---
+++ src/main/java/com/pramati/core/ValidationException.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.pramati.core;
+
+/**
+ * @author PAmbure
+ *
+ */
+@SuppressWarnings("serial")
+public class ValidationException extends Exception
+{
+	public ValidationException(String message)
+	{
+		super(message);
+	}
+}
